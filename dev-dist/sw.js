a2045452f607bf9c6f68296e68148a5b55beaa66
224ce11 <224ce11@svitvasad.ac.in>
2026-02-25 14:43:38 +0530

    Working product

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.rgftubcl3g8"
+    "revision": "0.diqh4c0gfoo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
