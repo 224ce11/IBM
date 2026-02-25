4a0bcbb49dd212bf07c55097302b1eea1fc528f6
224ce11 <224ce11@svitvasad.ac.in>
2026-02-25 15:06:07 +0530

    fix: add missing language dropdown CSS styles

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.g5fc0fqr388"
+    "revision": "0.nu0e4sji8no"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
