62a921ad8e8f37669f3a9c40e3d95263d3113a08
224ce11 <224ce11@svitvasad.ac.in>
2026-02-25 14:50:31 +0530

    Please work

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -79,7 +79,7 @@ define(['./workbox-5a5d9309'], (function (workbox) { 'use strict';
    */
   workbox.precacheAndRoute([{
     "url": "index.html",
-    "revision": "0.diqh4c0gfoo"
+    "revision": "0.g5fc0fqr388"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
